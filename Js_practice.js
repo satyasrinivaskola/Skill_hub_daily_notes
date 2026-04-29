5d5de6f1f4365870568b6ba64665fcfce5d43560
satyasrinivaskola <satyasrinivaskola@gmail.com>
2026-04-29 07:19:15 +0530

    29_04_26

---
+++ Js_practice.js
@@ -0,0 +1,18 @@
+
+const data=[22,44,1,55]
+const data1=[2,4,1,5]
+console.log(data.sort((a,b)=>a-b))
+data.push(9)
+console.log(data);
+data.pop()
+const res=data.concat(data1)
+console.log(res.sort((a,b)=>a-b));
+console.log(res.length)
+const userdetails={name:"satya",num:900}
+console.log(userdetails.name)
+for(let key in userdetails ){
+
+console.log(Object.keys(userdetails).length)
+}
+console.log(Object.keys(userdetails).length)
+
